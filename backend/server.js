8a5f9d7ca19a097e0b9e5efec5202ef485fd21e6
karnoark <mayureshkshirsagar2000@gmail.com>
2024-06-01 21:17:20 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import path from "path"
 import connectDB from "./db/connectDB.js"
 import userRoutes from "./routes/userRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
@@ -20,6 +21,7 @@ import {app, server} from "./socket/socket.js"
 connectDB()
 
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve() //__dirname is our root
 
 
 // middleware
@@ -31,12 +33,17 @@ app.use(cookieParser())
 app.use("/api/users", userRoutes)
 app.use("/api/messages", messageRoutes)
 
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static(path.join(__dirname, "/frontend/dist")))  //dist folder contains optimised version of our front end application, we are going to take this dist folder and make it our static asset
+	//if we hit any other routes than our api endpoint we are going to serve our react app
+	app.get("*", (req,res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+	})  
+}
+
 // start the server
 server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
-// app.listen(PORT, () => {
-//     console.log(`server is running on port ${PORT}`)
-// })
 
 
